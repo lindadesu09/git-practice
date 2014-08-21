c0cd1def893418e2db03ea9958aeea2f4c512e8a
Linda Chan <lindadesu09@gmail.com>
2014-08-21 10:10:19 +0800

    Revert "We didnt wnat this change"
    This reverts commit 4ddc63441c0d24989799d4d45bd3b0a9581db0d4.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,6 @@
-# Lab 16 Undoing committed changes
+# Lab19 Add an author comment & Email
+# Default is World
+# Author: Jim Weirich (jim@somewhere.com)
 name = ARGV.first || "World"
 
 puts "Hello, #{name}!"
