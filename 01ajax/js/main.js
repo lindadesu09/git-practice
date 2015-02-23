2c1e3f811fb9b80e3add628e2a4e1cf06b8a65b2
Linda Chan <lindadesu09@gmail.com>
2015-02-23 17:31:59 +0800

    2.1 the simples ajax script

---
+++ 01ajax/js/main.js
@@ -0,0 +1,50 @@
+// (function() {
+
+// 	var link = document.getElementsByTagName('a')[0];
+
+// 	link.onclick = function(e) {
+// 		// e.preventDefault();
+// 		return false;
+// 	}
+
+// })();
+
+(function() {
+
+	var link = document.getElementsByTagName("a")[0];
+
+	link.onclick = function() {
+		//XHR Object
+		var xhr = new XMLHttpRequest();
+
+		//handle the "onreadystatechange" event
+		//xhr.readyState property values
+		//0 = uninitalized
+		//1 = Loading
+		//2 = Loaded
+		//3 = Interactive
+		//4 = Complete
+
+		xhr.onreadystatechange = function() {
+			if ((xhr.readyState == 4) && (xhr.status == 200 || xhr.status == 304)) {
+				xhr.responseText;
+
+				var body = document.getElementsByTagName("body")[0];
+				var p = document.createElement("p");
+				var pText = document.createTextNode(xhr.responseText);
+				p.appendChild(pText);
+				body.appendChild(p);
+			}
+		};
+
+		//open the request
+		xhr.open("GET", "files/ajax.txt", true);
+		//send the request
+		xhr.send(null);
+
+		return false; // same as preventDefault();
+	};
+
+})();
+
+
